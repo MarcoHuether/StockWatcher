197bffde5f79c0b28fe83eebffc1046484f70630
Marco Huether <HuetherM@web.de>
2012-08-16 22:54:45 +0200

    source commit

---
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -0,0 +1,32 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class StockWatcher implements EntryPoint {
+
+  private VerticalPanel mainPanel = new VerticalPanel();
+  private FlexTable stocksFlexTable = new FlexTable();
+  private HorizontalPanel addPanel = new HorizontalPanel();
+  private TextBox newSymbolTextBox = new TextBox();
+  private Button addStockButton = new Button("Add");
+  private Label lastUpdatedLabel = new Label();
+
+  /**
+   * Entry point method.
+   */
+  public void onModuleLoad() {
+    // TODO Create table for stock data.
+    // TODO Assemble Add Stock panel.
+    // TODO Assemble Main panel.
+    // TODO Associate the Main panel with the HTML host page.
+    // TODO Move cursor focus to the input box.
+
+  }
+
+}
